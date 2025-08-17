5883660030f68bf90594c2186af0ec2e6d69aadb
Xinos <userxinos@gmail.com>
2025-08-18 00:52:39 +0700

    simplify

--- MyPlugin/src/components/MyComponent.vue
+++ MyPlugin/src/components/MyComponent.vue
@@ -3,9 +3,7 @@
 </script>
 
 <template>
-  <div>
-    <h1>{{ $t('helloWorld') }}</h1>
-  </div>
+
 </template>
 
 <style scoped>
--- MyPlugin/src/main.js
+++ MyPlugin/src/main.js
@@ -5,7 +5,7 @@ import ruLocale from '../locales/ru-RU.json';
 import MyComponent from './components/MyComponent.vue';
 
 // noinspection JSUnusedGlobalSymbols
-export default function plugin({ router, useStyle, defineSettings, defineLocales }) {
+export default function plugin({ useStyle, defineLocales }) {
   useStyle(styles);
 
   defineLocales({
@@ -13,36 +13,5 @@ export default function plugin({ router, useStyle, defineSettings, defineLocales
     'en-US': enLocale,
   });
 
-  router.addRoute({
-    name: 'MyComponentTest',
-    path: 'test',
-    component: MyComponent,
-  });
-
-  const url = ref();
-
-  defineSettings([
-    {
-      name: 'apiEndpoint',
-      ref: url,
-      type: 'url',
-    },
-    {
-      name: 'token',
-      description: 'tokenNote',
-      ref: toRef(null, 'token'),
-      type: 'text',
-    },
-    {
-      name: 'language',
-      description: 'languageNote',
-      ref: toRef(null, 'language'),
-      type: 'select',
-      options: [
-        { name: 'auto', value: '' },
-        { name: 'Русский', value: 'ru-RU' },
-        { name: 'English', value: 'en-US' },
-      ],
-    },
-  ]);
+  return () => null
 }
