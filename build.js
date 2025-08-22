17bff1fce1bf1816c9a38c0e47bfb811900184f7
Xinos <40886699+userXinos@users.noreply.github.com>
2025-08-22 21:02:56 +0700

    improve build, build to gh-pages

--- build.js
+++ build.js
@@ -1,17 +1,58 @@
 /* eslint-disable no-console */
 
 import { exec } from 'node:child_process';
+import { readdir, writeFile } from 'node:fs';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { promisify } from 'node:util';
 import { glob } from 'glob';
 
-glob('**/**/manifest.json')
-  .then((files) => files
+const execAsync = promisify(exec);
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// eslint-disable-next-line antfu/no-top-level-await
+await glob('**/**/manifest.json')
+  .then((files) => Promise.all(files
     .map((file) => file.replace('manifest.json', ''))
-    .map((target) => exec(`TARGET=${target} pnpx vite build`, (err, stdout, stderr) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
-    })),
+    .map((target) =>
+      execAsync(`TARGET=${target} pnpx vite build`)
+        .then(({ stdout, stderr }) => {
+          console.log(`stdout: ${stdout}`);
+          console.error(`stderr: ${stderr}`);
+        })
+        .catch((err) => {
+          console.error(err);
+        }),
+    )),
   );
+
+readdir(resolve(__dirname, 'dist'), (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const links = files
+    .map((f) => `<li><a href="./${f}">${f}</a></li>`)
+    .join('\n');
+
+  const html = `
+    <!DOCTYPE html>
+    <html lang="">
+    <head><meta charset="UTF-8" /><title>Yskra plugins</title></head>
+    <body style="display: flex;flex-direction: column;align-items: center;justify-content: center;">
+    <h1>Yskra plugins</h1>
+    <ul>${links}</ul>
+    </body>
+    </html>
+  `;
+
+  writeFile('dist/index.html', html, (writeErr) => {
+    if (writeErr) {
+      console.error(writeErr);
+    }
+    else {
+      console.log('dist/index.html');
+    }
+  });
+});
